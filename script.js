b6a0d62cf40b0dfc7540f96c3c6634a188f96adc
Blackfire529 <collinm2001@gmail.com>
2024-08-27 10:32:55 -0500

    Added_the_playRound_function

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ function getComputerChoice (computerChoice) {
 function getHumanChoice(humanChoice) {
     humanChoice = prompt('Enter Rock, Paper, or Scissors');
 }
+
+function playRound(humanChoice,computerChoice,humanScore,computerScore) {
+    humanChoice = humanChoice.toUpperCase();
+    computerChoice = computerChoice.toUpperCase();
+    if (humanChoice === ROCK && computerChoice === ROCK) {
+        console.log('Tie');
+    } else if (humanChoice === ROCK && computerChoice === PAPER) {
+        console.log('You lose! Paper beats Rock');
+        computerScore++;
+    } else if (humanChoice === ROCK && computerChoice === SCISSORS) {
+        console.log('You Win! Rock beats Scissors');
+        humanScore++;
+    } else if (humanChoice === PAPER && computerChoice === PAPER) {
+        console.log('Tie');
+    } else if (humanChoice === PAPER && computerChoice === ROCK) {
+        console.log('You Win! Paper beats Rock');
+        humanScore++;
+    } else if (humanChoice === PAPER && computerChoice === SCISSORS) {
+        console.log('You Lose! Scissors beats Paper');
+        computerScore++;
+    } else if (humanChoice === SCISSORS && computerChoice === SCISSORS) {
+        console.log('Tie');
+    } else if (humanChoice === SCISSORS && computerChoice === PAPER) {
+        console.log('You Win! Scissors beats Paper');
+        humanScore++;
+    } else if (humanChoice === SCISSORS && computerChoice === ROCK) {
+        console.log('You Lose! Rock beats Scissors');
+        computerScore++;
+    }
+    return;
+}
