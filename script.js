fcf9673bc8aeee4b8a22b968e7ec7297a9c14d5f
Blackfire529 <collinm2001@gmail.com>
2024-09-02 21:21:02 -0500

    Add_playGame_function_and_fixed_playRound_function

--- script.js
+++ script.js
@@ -8,19 +8,27 @@ function getComputerChoice (computerChoice) {
     } else {
         computerChoice = 'Scissors';
     }
-    return alert(computerChoice);
+    return computerChoice;
 }
 // a function to get the users choice of rock paper or scissors
 function getHumanChoice(humanChoice) {
     humanChoice = prompt('Enter Rock, Paper, or Scissors');
+    return humanChoice;
 }
 
-function playRound(humanChoice,computerChoice) {
+// a function to play a full game of rock paper scissors
+function playGame() {
     let computerScore = 0;
     let humanScore = 0;
-    
+    let rounds = 0;
+
+    // a function to play a round of rock paper scissors
+    function playRound(humanChoice,computerChoice) {
+        humanChoice = String(humanChoice).toUpperCase();
+        computerChoice = String(computerChoice).toUpperCase();
+
     if (humanChoice === computerChoice) {
-        alert('Tie');
+        console.log('No Winners Its a Tie :(')
     } else if (humanChoice === 'ROCK' && computerChoice === 'PAPER') {
         console.log('You lose! Paper beats Rock');
         computerScore++;
@@ -40,7 +48,19 @@ function playRound(humanChoice,computerChoice) {
         console.log('You lose! Rock beats Scissors');
         computerScore++;
     }
-    return console.log(`Final scores: Human-${humanScore} Computer-${computerScore}`);
+    
+    }
+    
+    // a loop to play 5 rounds of the game
+    while (rounds < 5) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection,computerSelection);
+        rounds++;
+    }
+    
+return console.log(`Final scores: Human-${humanScore} Computer-${computerScore}`);
 }
 
 
